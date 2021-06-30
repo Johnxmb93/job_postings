6a8f056267b9abdb7a92050941bf48acd3d93457
Jonathan Bahena <bahena@Jonathans-MacBook-Air.local>
2021-06-30 14:31:15 -0500

    added more to controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_admin, except: [:index, :show]
+  #before_action :authenticate_admin, except: [:index, :show]
   
   def index
     posts = Post.all
@@ -13,14 +13,15 @@ class PostsController < ApplicationController
       company_name: params[:company_name],
       salary: params[:salary],
       city: params[:city])
-      if posts.save
-        render json: posts
-      else
-        render json: {message: posts.errors.full_messages}, status: :bad_request
-      end
+    if posts.save
+      render json: posts
+    else
+      render json: {message: posts.errors.full_messages}, status: :bad_request
+    end
   end
   
   def show
+    
   end
  
   def update
